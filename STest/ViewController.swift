63f29a9c181a1837bb270cb596c391e2322125b1
Dmitry Kosyakov <this.kosyakovdmitry@gmail.com>
2020-09-09 14:07:48 +0300

    Initial Commit

---
+++ STest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STest
+//
+//  Created by Dmitry Kosyakov on 09.09.2020.
+//  Copyright © 2020 dev.bsl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
